5c2b45ec8f02aeb21767e2d688de8ef3252e841f
CecilioCervantes <ceciliocervantes@gmail.com>
2025-11-26 09:18:48 -0600

    Modify goal_time and return values for Mon–Thu
    I ADDED OVERRULE FOR MONTHUR TO ACT AS SAT

--- data_processor.py
+++ data_processor.py
@@ -494,8 +494,11 @@ def get_daily_time_goals(report_date):
     # Mon–Thu
     if weekday in [0, 1, 2, 3]:
         wrap_limit = 1.75 if is_commercial else 1.0
-        goal_time = 10 if is_prime else 9.5
-        return goal_time, 2 + 20/60, wrap_limit, 4.5, "08:00"
+        # goal_time = 10 if is_prime else 9.5
+        goal_time = 6
+        # return goal_time, 2 + 20/60, wrap_limit, 4.5, "08:00"
+        return goal_time, 1, wrap_limit, 2.75, "08:00"
+        
 
     # Friday
     elif weekday == 4:
