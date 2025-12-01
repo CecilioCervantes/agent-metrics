c143ca0e18a73c049c32cfa6ff88d69f30cbda8a
CecilioCervantes <ceciliocervantes@gmail.com>
2025-12-01 08:23:04 -0600

    Update data_processor.py
    back to normal

--- data_processor.py
+++ data_processor.py
@@ -507,11 +507,9 @@ def get_daily_time_goals(report_date):
 
     # Saturday
     elif weekday == 5:
-        # wrap_limit = 0.75
-        # return 6, 1, wrap_limit, 2.75, "08:00"
-        wrap_limit = 1.75 if is_commercial else 1.0
-        goal_time = 10 if is_prime else 9.5
-        return goal_time, 2 + 20/60, wrap_limit, 4.5, "08:00"
+        wrap_limit = 0.75
+        return 6, 1, wrap_limit, 2.75, "08:00"
+        
 
     # Sunday
     elif weekday == 6:
