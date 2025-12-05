87d855558aaf40c2ab0b2a83fafc6e1760c933dd
CecilioCervantes <ceciliocervantes@gmail.com>
2025-12-05 15:01:13 -0600

    Update data_processor.py
    w retano in commericla team

--- data_processor.py
+++ data_processor.py
@@ -768,7 +768,7 @@ def detect_inconsistencies(df):
 
 def classify_office(agent_name):
     commercial_agents = {
-        "sp tony", "sp allan", "sp chris", "sp jennifer", "sp mathew", "sp steve"
+        "sp tony", "sp allan", "sp chris", "sp jennifer", "sp mathew", "sp steve", "w retano"
     }
 
     if isinstance(agent_name, str):
