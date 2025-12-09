1efe463dd4140626103050c3359ea17780293788
CecilioCervantes <ceciliocervantes@gmail.com>
2025-12-09 17:05:26 -0600

    Update data_processor.py
    tigers instead of pakistan

--- data_processor.py
+++ data_processor.py
@@ -780,7 +780,7 @@ def classify_office(agent_name):
         if agent_name.startswith("sp ") or agent_name.startswith("pr "): return "Sp & Prime"
         if agent_name.startswith("e "): return "Egypt"
         if agent_name.startswith("s "): return "Spanish"
-        if agent_name.startswith("g "): return "Pakistan"
+        if agent_name.startswith("g "): return "Tigers"
     return "Other"
 
 
